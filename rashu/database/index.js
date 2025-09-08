b262948155ad8d4323d10d8b8b77d48b6c9e3142
BLACK MAFIA <100425878+Osadachami123@users.noreply.github.com>
2025-09-08 06:28:10 +0530

    Add files via upload

---
+++ rashu/database/index.js
@@ -0,0 +1,26 @@
+const {
+    saveContact,
+    loadMessage,
+    getName,
+    getChatSummary,
+    saveGroupMetadata,
+    getGroupMetadata,
+    saveMessageCount,
+    getInactiveGroupMembers,
+    getGroupMembersMessageCount,
+    saveMessage,
+} = require('./store');
+
+
+module.exports = {
+    saveContact,
+    loadMessage,
+    getName,
+    getChatSummary,
+    saveGroupMetadata,
+    getGroupMetadata,
+    saveMessageCount,
+    getInactiveGroupMembers,
+    getGroupMembersMessageCount,
+    saveMessage,
+};
